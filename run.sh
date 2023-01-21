27852f7e1e04f07e34f17263a26bccc35fd51e54
nachinolkar <66353133+nachinolkar@users.noreply.github.com>
2023-01-21 19:00:49 +0530

    vvv

--- run.sh
+++ run.sh
@@ -9,11 +9,10 @@ echo "netlify.toml" >>.gitignore
 rsync -a __site/zola/ __site/build
 rsync -a __site/content/ __site/build/content
 
-echo "reached here"
-
 # Use obsidian-export to export markdown content from obsidian
 mkdir -p __site/build/content/docs __site/build/__docs
 if [ -z "$STRICT_LINE_BREAKS" ]; then
+	echo "reached here"
     chmod a+x __site/bin/obsidian-export
 	__site/bin/obsidian-export --frontmatter=never --hard-linebreaks --no-recursive-embeds __obsidian __site/build/__docs
 else
