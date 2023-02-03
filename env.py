721d6f8127702b06bc06d7cddef870ffd9613051
nachinolkar <66353133+nachinolkar@users.noreply.github.com>
2023-02-03 22:22:56 +0530

    summary

--- env.py
+++ env.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import rtoml
 
 if __name__ == "__main__":
-    env_vars = rtoml.load(Path(environ["VAULT"]) / "netlify.toml")["build"][
+    env_vars = rtoml.load("C:\Users\tin0976\OneDrive - Volvo Group\Personal\08-Writing\Vaults\Thoughts At Fifty\netlify.toml")["build"][
         "environment"
     ]
     for k, v in env_vars.items():
