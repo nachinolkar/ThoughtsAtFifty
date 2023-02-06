3e2225ea39be658f9610d0a71ed92b69e86cd2fd
nachinolkar <66353133+nachinolkar@users.noreply.github.com>
2023-02-06 19:11:14 +0530

    summ

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ pip install python-slugify
 echo "netlify.toml" >>.gitignore
 
 # Sync Zola template contents
-rsync -a __site/zola/ __site/build
-rsync -a __site/content/ __site/build/content
+rsync -v __site/zola/ __site/build
+rsync -v __site/content/ __site/build/content
 
 # Use obsidian-export to export markdown content from obsidian
 mkdir -p __site/build/content/docs __site/build/__docs
@@ -18,6 +18,8 @@ if [ -z "$STRICT_LINE_BREAKS" ]; then
 else
 	__site/bin/obsidian-export --frontmatter=never --no-recursive-embeds __obsidian __site/build/__docs
 fi
+
+ls
 
 # Run conversion script
 python __site/convert.py
